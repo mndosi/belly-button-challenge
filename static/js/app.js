66a90daf108f61cc309f1851adeae19a7851e965
mndosi <mankwendosisings@gmail.com>
2023-01-13 22:33:11 -0600

    Bubble Chart Finished

--- static/js/app.js
+++ static/js/app.js
@@ -63,82 +63,132 @@ let y_axis = otu_ids.slice(0,10).map(otu_id => `OTU ${otu_id}`).reverse();
   };
 // Render the bar plot to the div tag with id "plot"
 // Note that we use `traceData` here, not `data`
-Plotly.newPlot("bar", traceData, layout)
-});
+Plotly.newPlot("bar", traceData, layout);
 
 // Working the Bubble Chart
+let bubbleData = [
+  {
+    x: otu_ids,
+    y: sample_values,
+    text: labels,
+    mode: "markers",
+    marker: {
+      size: sample_values,
+      color: otu_ids,
+      colorscale: "Earth"
+    }
+  }
+];
 
-// function buildBubbleChart(sample) {
+// Deliverable 2: 2. Create the layout for the bubble chart.
+let bubbleLayout = {
+  title: "Bacteria Cultures Per Sample",
+  margin: { t: 0 },
+  hovermode: "closest",
+  xaxis: { title: "OTU ID" },
+  margin: { t: 30}
+};
 
-// Read the data
-d3.json(sample_data).then((data)=> {
-  console.log(data);
+// Deliverable 2: 3. Use Plotly to plot the data with the layout.
+Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-let sample_Array = data.samples;
 
-let myArray = sample_Array.filter(sampleid => sampleid.id == sample);
-let first_sample = myArray[0];
+// to Display the Demographic Metadata information
+
+// setting up variables to hold the demographic information
+
+let id = first_sample.id;
+let ethnicity = first_sample.ethnicity;
+let gender = first_sample.gender;
+let age = first_sample.age;
+let location = first_sample.location;
+let bbtype = first_sample.bbtype;
+let wfreq = first_sample.wfreq;
+
+let myPanel = {
+    "id: ": otu_ids,
+    "ethnicity: ": ethnicity,
+    "gender: ": gender,
+    "age: ": age,
+    "location: ": location,
+    "bbtype: ": bbtype,
+    "wfreq:": wfreq,
+}
+});
+}
+
+
+// // function buildBubbleChart(sample) {
+
+// // Read the data
+// d3.json(sample_data).then((data)=> {
+//   console.log(data);
+
+// let sample_Array = data.samples;
+
+// let myArray = sample_Array.filter(sampleid => sampleid.id == sample);
+// let first_sample = myArray[0];
   
-let otu_ids = first_sample.otu_ids;
-let labels = first_sample.otu_labels;
-let sample_values = first_sample.sample_values;
+// let otu_ids = first_sample.otu_ids;
+// let labels = first_sample.otu_labels;
+// let sample_values = first_sample.sample_values;
 
-// let bubble_plot = d3.select('#bubble'),
-// d3.selectAll("circle"),
-//     .data(data).enter()
+// // let bubble_plot = d3.select('#bubble'),
+// // d3.selectAll("circle"),
+// //     .data(data).enter()
 
-// let traceData2 = [trace2];
+// // let traceData2 = [trace2];
+
+// // let trace2 = {
+// //   d3.
+// //   x: otu_ids,
+// //   y: sample_values,
+// //   marker_size: sample_values
+// //   marker_colors: otu_ids
+// //   text: labels,
+// //   type: "bubble",
+// //   orientation: "h"
+// // };
+ 
+// // let margin = {top: 10, right: 20, bottom: 30, left: 50},
+// //     width = 500 - margin.left - margin.right,
+// //     height = 420 - margin.top - margin.bottom;
+
+
+// let mycolor = otu_ids.scaleLinear().domain([1,10])
+// .range(["white", "red"])
+
+// // myBubble.selectAll(sample_data(data).enter()
+// //   .append("circle")
+// //   .attr("cx", function(d,i){return 30 + i*60})
+// //   .attr("cy", 50)
+// //   .attr("r", 19)
+// //   .attr("fill", function(d){return myColor(d) }));
 
 // let trace2 = {
-//   d3.
 //   x: otu_ids,
 //   y: sample_values,
-//   marker_size: sample_values
-//   marker_colors: otu_ids
 //   text: labels,
-//   type: "bubble",
-//   orientation: "h"
+//   mode: 'markers',
+//   marker: {
+//     color: mycolor,
+//     opacity: [0.6],
+//     size: sample_values
+//   }
 // };
- 
-// let margin = {top: 10, right: 20, bottom: 30, left: 50},
-//     width = 500 - margin.left - margin.right,
-//     height = 420 - margin.top - margin.bottom;
+// let traceData2 = [trace2];
 
-
-let mycolor = otu_ids.scaleLinear().domain([1,10])
-.range(["white", "red"])
-
-// myBubble.selectAll(sample_data(data).enter()
-//   .append("circle")
-//   .attr("cx", function(d,i){return 30 + i*60})
-//   .attr("cy", 50)
-//   .attr("r", 19)
-//   .attr("fill", function(d){return myColor(d) }));
-
-let trace2 = {
-  x: otu_ids,
-  y: sample_values,
-  text: labels,
-  mode: 'markers',
-  marker: {
-    color: mycolor,
-    opacity: [0.6],
-    size: sample_values
-  }
-};
-let traceData2 = [trace2];
-
-let layout = {
-  title: 'Belly Button Bacteria per Sample ',
-  hovermode: "closest",
-  showlegend: false,
-  xaxis: {title: "Bacterial Species ID Number"},
-  yaxis: {title: "Amount Present in Culture"}
-};
-//Call Plotly to plot the chart on the page
-Plotly.newPlot('bubble', traceData2, layout);
-})
-};
+// let layout = {
+//   title: 'Belly Button Bacteria per Sample ',
+//   hovermode: "closest",
+//   showlegend: false,
+//   xaxis: {title: "Bacterial Species ID Number"},
+//   yaxis: {title: "Amount Present in Culture"}
+// };
+// //Call Plotly to plot the chart on the page
+// Plotly.newPlot('bubble', traceData2, layout);
+// })
+// };
 
 
 
